959913dc621acba12ddca66adecf58b4d0fdbdda
Denis <contato.denismedeiros@hotmail.com>
2020-10-21 08:30:55 -0300

    init mobile expo

---
+++ mobile/src/pages/MapSearch.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from '../styles/pages/mapSearch';
+
+export default function MapSearch() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello world!!</Text>
+    </View>
+  );
+}
