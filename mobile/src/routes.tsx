fba2bcb1709160711c3fb28695381fbb6cee90ce
Denis <contato.denismedeiros@hotmail.com>
2020-10-21 19:54:20 -0300

    add routes

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import MapSearch from './pages/MapSearch';
+import TreeFallData from './pages/CreateTreeFall/TreeFallData';
+import SelectMapPosition from './pages/CreateTreeFall/SelectMapPosition';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -10,7 +12,17 @@ export default function Routes() {
   return (
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
-        <Screen name="mapsearch" component={MapSearch} />
+        <Screen name="mapSearch" component={MapSearch} />
+        <Screen
+          name="SelectMapPosition"
+          component={SelectMapPosition}
+          options={{ headerShown: true, title: 'Selecione no mapa' }}
+        />
+        <Screen
+          name="treeFallData"
+          component={TreeFallData}
+          options={{ headerShown: true, title: 'Informe os dados' }}
+        />
       </Navigator>
     </NavigationContainer>
   );
