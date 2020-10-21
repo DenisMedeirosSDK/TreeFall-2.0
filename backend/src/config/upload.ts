0e42adbe8413463bf60f7631177b7629007c1268
Denis <contato.denismedeiros@hotmail.com>
2020-10-21 08:26:18 -0300

    fix upload

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -4,7 +4,7 @@ import multer from 'multer';
 export default {
   storage: multer.diskStorage({
     destination: path.join(__dirname, '..', '..', 'uploads'),
-    filename: (_, file, callback) => {
+    filename: (request, file, callback) => {
       const filename = `${Date.now()}-${file.originalname}`;
 
       callback(null, filename);
