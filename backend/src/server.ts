dc3d116a913a7987772d326336dbec971ff486a0
Denis <contato.denismedeiros@hotmail.com>
2020-10-21 08:25:57 -0300

    add error to server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,11 +5,14 @@ import path from 'path';
 import 'express-async-errors';
 import './database/connections';
 
+import errorHandler from './errors/handler';
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+
+app.use(errorHandler);
 
 app.listen(3333, () => {
   console.log('Server listening on port 3333');
