8792ae01e705691a1614325fc3502770abd3e178
Denis <contato.denismedeiros@hotmail.com>
2020-10-21 19:54:32 -0300

    add css

--- mobile/src/styles/pages/mapSearch.ts
+++ mobile/src/styles/pages/mapSearch.ts
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -6,6 +6,39 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  footer: {
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 32,
+
+    backgroundColor: '#FFF',
+    borderRadius: 20,
+    height: 56,
+    paddingLeft: 24,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+    elevation: 3,
+  },
+  footerText: {
+    color: '#8fa7b3',
+  },
+  createTreeFallButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: '#7DC81C',
+    borderRadius: 20,
+
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
